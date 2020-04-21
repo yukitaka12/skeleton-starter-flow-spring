2049a655bcd945daed0f9eaca89dfed086bf6a62
Johannes Eriksson <joheriks@vaadin.com>
2020-04-21 12:49:19 +0300

    UI for exercising notifications

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -1,30 +1,30 @@
 package org.vaadin.example;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.flow.component.AttachEvent;
-import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 /**
  * A sample Vaadin view class.
  * <p>
- * To implement a Vaadin view just extend any Vaadin component and
- * use @Route annotation to announce it in a URL as a Spring managed
- * bean.
- * Use the @PWA annotation make the application installable on phones,
- * tablets and some desktop browsers.
+ * To implement a Vaadin view just extend any Vaadin component and use @Route
+ * annotation to announce it in a URL as a Spring managed bean. Use the @PWA
+ * annotation make the application installable on phones, tablets and some
+ * desktop browsers.
  * <p>
- * A new instance of this class is created for every new user and every
- * browser tab/window.
+ * A new instance of this class is created for every new user and every browser
+ * tab/window.
  * <p>
  * The main view contains a text field for getting the user name and a button
  * that shows a greeting message in a notification.
@@ -32,7 +32,7 @@ import com.vaadin.flow.router.Route;
 @Route
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-@NpmPackage(value = "lit-element", version="2.3.1")
+@NpmPackage(value = "lit-element", version = "2.3.1")
 @JsModule("./vaadin-devmode-gizmo.js")
 public class MainView extends VerticalLayout {
 
@@ -41,29 +41,59 @@ public class MainView extends VerticalLayout {
      * <p>
      * Build the initial UI state for the user accessing the application.
      *
-     * @param service The message service. Automatically injected Spring managed bean.
+     * @param service
+     *            The message service. Automatically injected Spring managed
+     *            bean.
      */
     public MainView(@Autowired GreetService service) {
+        ComboBox<String> typeField = new ComboBox<>("Type");
+        typeField.setItems(Arrays.asList("information", "warning"));
+        typeField.setValue("information");
+        add(typeField);
 
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
+        TextField messageField = new TextField("Message");
+        messageField.setValue("Test notification");
+        add(messageField);
 
-        // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello",
-                e -> Notification.show(service.greet(textField.getValue())));
+        TextArea detailsField = new TextArea("Details");
+        add(detailsField);
 
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button is more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        TextField linkField = new TextField("Link URL");
+        add(linkField);
 
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
+        TextField persistenceIdField = new TextField("Persistence ID");
+        add(persistenceIdField);
 
-        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-        addClassName("centered-content");
+        Button showNotification = new Button("Show notification", e -> {
+            String type = "'" + typeField.getValue() + "'";
+            String message = jsString(messageField.getValue());
+            String details = detailsField.isEmpty() ? "null"
+                    : jsString(detailsField.getValue());
+            String link = linkField.isEmpty() ? "null"
+                    : jsString(linkField.getValue());
+            String persistenceId = persistenceIdField.isEmpty() ? "null"
+                    : jsString(persistenceIdField.getValue());
+            getUI().get().getPage()
+                    .executeJs(String.format(
+                            "window.Gizmo.showNotification(%s, %s, %s, %s, %s)",
+                            type, message, details, link, persistenceId));
+        });
+        add(showNotification);
 
-        add(textField, button);
+        TextField splashMessageField = new TextField("Splash message");
+        splashMessageField.setValue("Test splash message");
+        add(splashMessageField);
+
+        Button showSplashMessage = new Button("Show splash message", e -> {
+            getUI().get().getPage().executeJs(
+                    String.format("window.Gizmo.showSplashMessage(%s)",
+                            jsString(splashMessageField.getValue())));
+        });
+        add(showSplashMessage);
+    }
+
+    private static String jsString(String s) {
+        return "'" + s.replace("'", "\\'").replace("\n", "\\n") + "'";
     }
 
     protected void onAttach(AttachEvent e) {
@@ -71,7 +101,8 @@ public class MainView extends VerticalLayout {
                 "let gizmo = document.createElement('vaadin-devmode-gizmo');"
                         + "gizmo.setAttribute('springbootdevtoolsport','35729');"
                         + "gizmo.setAttribute('liveReloadBackend','SPRING_BOOT_DEVTOOLS');"
-                        + "document.body.appendChild(gizmo);");
+                        + "document.body.appendChild(gizmo);"
+                        + "window.Gizmo = gizmo;");
     }
 
 }
