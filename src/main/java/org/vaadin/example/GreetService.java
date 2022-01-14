01df1c9bcb857aee3b33c8a3af5af2bef1e3165d
yukitaka12 <smyktk12@gmail.com>
2022-01-14 14:21:02 +0000

    updated

--- src/main/java/org/vaadin/example/GreetService.java
+++ src/main/java/org/vaadin/example/GreetService.java
@@ -4,14 +4,30 @@ import java.io.Serializable;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
+
 @Service
 public class GreetService implements Serializable {
+    double n=0;
+    int number;
 
-    public String greet(String name) {
-        if (name == null || name.isEmpty()) {
-            return "Hello anonymous user";
+    public String greet(String name,String contents) {
+        Date dateObj = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        String display = format.format(dateObj);
+        //LocalDateTime nowDateTime = LocalDateTime.now();
+        //DateTimeFormatter java8Format = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        //String java8Disp = nowDateTime.format( java8Format );
+        if (name == null || name.isEmpty() || contents == null || contents.isEmpty()) {
+            return null;
         } else {
-            return "Hello " + name;
+            n += 0.5;
+            int number = (int) n;
+            
+            return number + " 名前 : " + name + " : " + display;
         }
     }
 
--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -1,15 +1,22 @@
 package org.vaadin.example;
 
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.board.Board;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
 
 /**
  * A sample Vaadin view class.
@@ -30,7 +37,7 @@ import org.springframework.beans.factory.annotation.Autowired;
         enableInstallPrompt = false)
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-public class MainView extends VerticalLayout {
+public class MainView extends VerticalLayout{
 
     /**
      * Construct a new Vaadin view.
@@ -42,25 +49,57 @@ public class MainView extends VerticalLayout {
     public MainView(@Autowired GreetService service) {
 
         // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
+        VerticalLayout List = new VerticalLayout();
+        TextField textField = new TextField("名前:");
         textField.addThemeName("bordered");
+        //TextField content = new TextField();
 
+        TextArea textArea = new TextArea();
+        textArea.setMaxLength(10000);
+        textArea.setValueChangeMode(ValueChangeMode.EAGER);
+        textArea.addValueChangeListener(e -> {
+            e.getSource().setHelperText(e.getValue().length() + "/" + 10000);
+        });
+        
         // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello",
-                e -> Notification.show(service.greet(textField.getValue())));
+
+        Button button = new Button("書き込む");
+        button.addClickListener(click -> {
+            if(service.greet(textField.getValue(),textArea.getValue())==null){
+            Notification.show("名前か送信内容が入力されていません。");
+            }
+
+
+            else{
+            List.add(new H3(service.greet(textField.getValue(),textArea.getValue())),new H3(textArea.getValue()));
+            }
+        });
+
+        //Button button = new Button("書き込む",
+                //e -> new H1(service.greet(textField.getValue(),content.getValue())));
+                
+        //Notification.show(service.greet(textField.getValue(),content.getValue())));
 
         // Theme variants give you predefined extra styles for components.
         // Example: Primary button has a more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        //button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
         // You can specify keyboard shortcuts for buttons.
         // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
+        //button.addClickShortcut(Key.ENTER);
 
         // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-        addClassName("centered-content");
+        textArea.addClassName("centered-content");
+        textField.addClassName("centered-content");
 
-        add(textField, button);
+        add(
+            new H1("Vaadin thread"),
+            List,
+            new HorizontalLayout(
+                button
+            ),
+            textField,
+            textArea
+        );
     }
-
 }
